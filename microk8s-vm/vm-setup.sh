68c922e766f5a1881ec6110b788e316009cabc8a
root <root@ansible.localdomain>
2024-03-09 14:48:28 +0000

    .

--- microk8s-vm/vm-setup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-lxc launch ubuntu:20.04 --vm -c limits.cpu=2 -c limits.memory=4GiB node1
-lxc launch ubuntu:20.04 --vm -c limits.cpu=2 -c limits.memory=4GiB node2
-lxc launch ubuntu:20.04 --vm -c limits.cpu=2 -c limits.memory=4GiB node3
-lxc launch ubuntu:20.04 --vm -c limits.cpu=2 -c limits.memory=4GiB node4
